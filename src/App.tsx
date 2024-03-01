61659edd9a722860ae8cac61dae2588c54954196
Tobias Andersson <tobiaserikandersson@gmail.com>
2024-03-01 19:33:19 +0200

    svgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 
+import fullSvg from "./assets/fullpage.svg"
 
 function App() {
 
   return (
     <>
       <h1>app</h1>
+      <img src={fullSvg} alt="Full Page SVG" />
     </>
   )
 }
