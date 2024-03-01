dfd852bc98dc91190bce0a0ed062fbe6a8e6b841
Tobias Andersson <tobiaserikandersson@gmail.com>
2024-03-01 20:38:07 +0200

    structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import About from "./pages/About";
+import Landing from "./pages/Landing";
+import Menu from "./pages/Menu";
+import Profile from "./pages/Profile";
+import Status from "./pages/Status";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Landing />} />
+      <Route path="/Menu" element={<Menu />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Profile" element={<Profile />} />
+      <Route path="/Status" element={<Status />} />
+    </Route>
+  )
+);
+
+
 import './App.css'
 
-import fullSvg from "./assets/fullpage.svg"
+
 
 function App() {
 
   return (
     <>
-      <h1>app</h1>
-      <img src={fullSvg} alt="Full Page SVG" />
+     <h1>app</h1>
+      <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,15 @@
+import "../abstracts/landing.scss"
+
+import Logo from "../assets/logo.svg"
+
+const Landing = () => {
+  return (
+    <>
+    <div>Landing</div>
+    <img src={Logo} alt="logo"/>
+    </>
+    
+  )
+}
+
+export default Landing
